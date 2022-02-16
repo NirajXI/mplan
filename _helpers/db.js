638dfd6b18d5fee9b45bfcd08a9433ddc01a85b7
veridate-n-wijerathnam-at-250450682494 <n.wijerathnam@veridatefinancial.com>
2022-02-16 10:07:14 +0530

    main db connect

--- _helpers/db.js
+++ _helpers/db.js
@@ -8,13 +8,12 @@ initialize();
 
 async function initialize() {
     // create db if it doesn't already exist
-    const { host, port, user, password, database } = config.database;
-    console.log(host);
+    const { host, port, user, password, database,dialect } = config.database;
     const connection = await mysql.createConnection({ host, port, user, password });
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    const sequelize = new Sequelize(database, user, password, {  host: host, dialect: 'mysql' });
 
     // init models and add them to the exported db object
     db.User = require('../users/user.model')(sequelize);
